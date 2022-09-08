fef0a725c8c06a2193285ec2169861c1df0814da
Faithful Chekwas Soruchi <faithfulchekwas@gmail.com>
2022-09-08 15:33:12 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#incude <stdio.h>
+
+/**
+ * main - print the string in the put function
+ * description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * return: 0
+ */
+int main(void)
+{
+	puts(*\*Programming is like building a multilingual puzzle*);
+	return (0);
+}
