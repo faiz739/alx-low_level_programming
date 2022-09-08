a0513f5cae6b7f271236f1afac7584f04439c90c
Faithful Chekwas Soruchi <faithfulchekwas@gmail.com>
2022-09-08 16:07:35 -0700

    code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - print the string in the put function
  *
- * description: using the main function
+ * Description: using the main function
  * this program prints "Programming is like building a multilingual puzzle
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle":);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
