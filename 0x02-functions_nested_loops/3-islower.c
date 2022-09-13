5b0ed4f0376d965783a930e94b2f1c93401958de
Faithful Chekwas Soruchi <faithfulchekwas@gmail.com>
2022-09-13 14:36:17 -0700

    t3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,11 +5,10 @@
 
  * @c: the character to check
 
- * Return: 1 if c is lowercase, 0 otherwise
+ * teturn: 1 if c is lowercase, 0 otherwise
  */
 
 int _islower(int c)
-
 {
 
 	return (c >= 'a' && c <= 'z');
