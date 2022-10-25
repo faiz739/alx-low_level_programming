ffe485b4a4293c8acc9f642746ded2d843106a9b
Faithful Chekwas Soruchi <faithfulchekwas@gmail.com>
2022-10-25 12:40:06 -0700

    more

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 #include <stdlib.h>
 
-/**add_nodeint - adds a new node at the beginning of a listint_t list.
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
  * @head: douuble pointer to the beginning of a listint_t list
  * @n: integer to add to the list
  *
